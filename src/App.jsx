40067522bfbf9c2f34a64558a1ed00795e0b61c2
Bob <teemu9598@gmail.com>
2023-10-09 22:29:43 +0300

    Realise better way to extract reset from useField object

--- src/App.jsx
+++ src/App.jsx
@@ -50,21 +50,18 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
+  const {reset: contentReset, ...content} = useField('text')
+  const {reset: authorReset, ...author} = useField('text')
+  const {reset: infoReset, ...info} = useField('text')
 
   const navigate = useNavigate()
 
   const resetFields = (e) => {
     e.preventDefault()
-    content.reset()
-    author.reset()
-    info.reset()
+    contentReset()
+    authorReset()
+    infoReset()
   }
-
-  const removeProperty = () => ({['reset']:remove, ...rest}) => rest
-  const removeReset = removeProperty('reset')
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -81,15 +78,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' {...removeReset(content)} />
+          <input name='content' {...content} />
         </div>
         <div>
           author
-          <input name='author' {...removeReset(author)} />
+          <input name='author' {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' {...removeReset(info)} />
+          <input name='info' {...info} />
         </div>
         <button>create</button>
         <button onClick={resetFields}>reset</button>
